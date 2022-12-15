bfb3de0ad83579caf0480e2f7fd13792e7500e1a
Faustina Maria <faustina.giaquinta@gmail.com>
2022-12-15 16:30:38 +0100

    adding script to fetch daily input

---
+++ fetch_daily_input.py
@@ -0,0 +1,17 @@
+import requests
+import os
+import sys
+from dotenv import load_dotenv
+load_dotenv()
+
+COOKIES = {'session': os.environ.get('AOC_SESSION')}
+
+
+input_url = 'https://adventofcode.com/2022/day/{}/input'.format(sys.argv[1])
+response = requests.get(input_url, cookies=COOKIES)
+file_name = 'input_d{}.txt'.format(sys.argv[1])
+
+if response.status_code and not os.path.exists(file_name):
+    file = open(file_name, 'wb')
+    file.write(response.content)
+    file.close()
