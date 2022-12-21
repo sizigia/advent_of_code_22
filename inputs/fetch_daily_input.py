3706aff6a2fdce3832339fa1d28fb83c87cfa5dc
Faustina Maria <faustina.giaquinta@gmail.com>
2022-12-21 14:54:07 +0100

    updating fetch_daily_input script to save files on the inputs/ folder

--- inputs/fetch_daily_input.py
+++ inputs/fetch_daily_input.py
@@ -9,7 +9,7 @@ COOKIES = {'session': os.environ.get('AOC_SESSION')}
 
 input_url = 'https://adventofcode.com/2022/day/{}/input'.format(sys.argv[1])
 response = requests.get(input_url, cookies=COOKIES)
-file_name = 'input_d{}.txt'.format(sys.argv[1])
+file_name = os.path.join('inputs/', 'input_d{}.txt'.format(sys.argv[1]))
 
 if response.status_code and not os.path.exists(file_name):
     file = open(file_name, 'wb')
