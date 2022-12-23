a87a5701db72a1db27a6a1caf72eecb949242cee
Faustina Maria <faustina.giaquinta@gmail.com>
2022-12-23 23:43:44 +0100

    adding day 3 - part 2, refactored part 1 by creating a function to find the common item in two or more rucksacks and returns the priority count

--- day_3.py
+++ day_3.py
@@ -24,30 +24,67 @@ The fifth rucksack's compartments only share item type t.
 The sixth rucksack's compartments only share item type s.
 To help prioritize item rearrangement, every item type can be converted to a priority:
 
-Lowercase item types a through z have priorities 1 through 26.
-Uppercase item types A through Z have priorities 27 through 52.
+Lowercase item types a through z have priorities_p1 1 through 26.
+Uppercase item types A through Z have priorities_p1 27 through 52.
 In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
 
-Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities_p1 of those item types?
+
+--- Part Two ---
+As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+And the second group's rucksacks are the next three lines:
+
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+priorities_p1 for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities_p1 of those item types?
 """
 import string
-from collections import Counter
 
 priority = string.ascii_letters
-
-priorities = 0
+priorities_p1 = 0
 
 filename = 'inputs/input_d3.txt'
 with open(filename) as file:
     lines = file.readlines()
     file.close()
 
+def find_common_item_in_rucksacks(rucksacks, priorities_count):
+    common_item = list(set.intersection(*map(set, rucksacks)))[0]
+    priorities_count += priority.index(common_item) + 1
+    return priorities_count
+
 for line in lines:
     rucksack_size = line.__len__()//2
     rucksack_1 = line[:rucksack_size]
     rucksack_2 = line[rucksack_size:]
 
-    common_item = [item for item in rucksack_1 if item in rucksack_2][0]
-    priorities += priority.index(common_item) + 1
+    priorities_p1 = find_common_item_in_rucksacks([rucksack_1, rucksack_2], priorities_p1)
 
-print(f"The sum of the priorities of the item types, that appear in both compartments of each rucksack, is {priorities}.")
+print(f"Part ONE - The sum of the priorities of the item types, that appear in both compartments of each rucksack, is {priorities_p1}.")
+
+priorities_p2 = 0
+
+for group in range(3, len(lines) + 3, 3):
+    # rucksack_1, rucksack_2, rucksack_3 = [rs.strip() for rs in lines[group - 3:group]]
+    rucksacks = [rs.strip() for rs in lines[group - 3:group]]
+    
+    priorities_p2 = find_common_item_in_rucksacks(rucksacks, priorities_p2)
+
+print(f"Part TWO - The sum of the priorities of the item types, that appear in both compartments of each rucksack, is {priorities_p2}.")
