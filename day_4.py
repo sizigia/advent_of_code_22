b041a3f6759ffd6ddc8f03dd3f487d0794fdfce5
Faustina Maria <faustina.giaquinta@gmail.com>
2022-12-24 02:10:17 +0100

    adding day 4 - part 2

--- day_4.py
+++ day_4.py
@@ -39,6 +39,19 @@ This example list uses single-digit section IDs to make it easier to draw; your 
 Some of the pairs have noticed that one of their assignments fully contains the other. For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6. In pairs where one assignment fully contains the other, one Elf in the pair would be exclusively cleaning sections their partner will already be cleaning, so these seem like the most in need of reconsideration. In this example, there are 2 such pairs.
 
 In how many assignment pairs does one range fully contain the other?
+
+--- Part Two ---
+It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+
+In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+
+5-7,7-9 overlaps in a single section, 7.
+2-8,3-7 overlaps all of the sections 3 through 7.
+6-6,4-6 overlaps in a single section, 6.
+2-6,4-8 overlaps in sections 4, 5, and 6.
+So, in this example, the number of overlapping assignment pairs is 4.
+
+In how many assignment pairs do the ranges overlap?
 """
 from helpers import retrieve_input
 
@@ -64,3 +77,20 @@ for pair in lines:
         assignments_fully_contained += 1
 
 print(f"Part ONE - There are {assignments_fully_contained} assignment pairs where one range fully contain the other.")
+
+assignments_overlap = 0
+
+def find_sections_overlap(first_elf, second_elf):
+    case_a = second_elf['first_section'] >= first_elf['last_section'] \
+         and second_elf['last_section'] <= first_elf['first_section']
+    case_b = second_elf['first_section'] <= first_elf['last_section'] \
+         and second_elf['last_section'] >= first_elf['first_section']
+    return case_a or case_b
+
+for pair in lines:
+    first_elf, second_elf = [create_elf_section_dict(sections) for sections in pair.strip().split(',')]
+    
+    if find_sections_overlap(first_elf, second_elf):
+        assignments_overlap += 1
+
+print(f"Part TWO - There are {assignments_overlap} assignment pairs where the ranges overlap.")
